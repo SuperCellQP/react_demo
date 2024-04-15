5221375d41a98c6401034420b6e750f0ffa177cb
SuperCellQP <151821552+SuperCellQP@users.noreply.github.com>
2024-04-15 16:23:58 +0700

    demo2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-
+import { useState } from 'react';
 
 function App() {
+  const [work, setWork] = useState('')
+  console.log(work)
   return (
-    <div className="App">
-      app
+    <div className="flex gap-8 h-screen items-center border border-red-500 justify-center">
+      <input className="outline-none border border-blue-500 px-4 py-2" value={work} onChange={e => setWork(e.target.value)}></input>
+      <button type="button" className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white">Add</button>
     </div>
   );
 }
